d32d5cc82b03fcd5a50ee078f28e87f8a764fe1b
Sathwik S rao <raosathwik889@gmail.com>
2026-04-17 23:22:03 +0530

    Add files via upload

---
+++ LinkedlistPrograms_1c_5.java
@@ -0,0 +1,25 @@
+//Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using Collections.swap(l_list, 0, 2))
+
+package Linkedlistexercise;
+import java.util.*;
+public class LinkedlistPrograms_1c_5 {
+	
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			LinkedList<String> ls = new LinkedList<>();
+			ls.add("Orange");
+			ls.add("geen");
+			ls.add("pink");
+			ls.add("red");
+			ls.add("Blue");
+			System.out.println("Before swapping");
+			System.out.println(ls);
+			Collections.swap(ls, 2, 3);
+			System.out.println("After swapping");
+			System.out.println(ls);
+		}
+
+
+}
