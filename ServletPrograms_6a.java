87316e27c14229b553904e44d3272f080881edbb
Sathwik S rao <raosathwik889@gmail.com>
2026-04-18 23:56:53 +0530

    Update ServletPrograms_6a.java

--- ServletPrograms_6a.java
+++ ServletPrograms_6a.java
@@ -1,4 +1,4 @@
-//Build a servlet program to find the factorial of a number using HTML with step by step procedure.
+//Execute the following Hello World ! Servlet Worked out Example
 import java.io.IOException;
 import java.io.PrintWriter;
 
